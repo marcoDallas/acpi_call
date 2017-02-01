cc146764e31a2f9f0a91a15b9efbf3273fc01e30
Marco Dalla Libera <marcoDallas@users.noreply.github.com>
2017-02-01 21:46:24 +0100

    Fix for bash 4.4, uncommented break when a code is found
    Now removes null bytes from the output of the cat command to avoid bash
    4.4 to send warnings. This fix should be ok, before version 4.4 bash
    was already silently removing null bytes.
    
    Since the GUI utilities save the first working code that is found I
    uncommented the break command, so the script can stop and limit the number
    of acpi calls.

--- examples/turn_off_gpu.sh
+++ examples/turn_off_gpu.sh
@@ -36,14 +36,14 @@ methods="
 for m in $methods; do
     echo -n "Trying $m: "
     echo $m > /proc/acpi/call
-    result=$(cat /proc/acpi/call)
+    result=$(cat /proc/acpi/call | tr -d \\0) #for bash 4.4: removes null bytes
     case "$result" in
         Error*)
             echo "failed"
         ;;
         *)
             echo "works!"
-            # break # try out outher methods too
+            break #stop at first working code
         ;;
     esac
 done
